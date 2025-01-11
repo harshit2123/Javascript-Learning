f17def8b021dfe1c00cc9924338f1663c7441d49
Harshit kumar yadav <harshitkmryadav@gmail.com>
2025-01-12 02:06:08 +0530

    5 videos Completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = "harshitkmryadav#gmail.com";
+var accountPassword = "1234";
+
+accountstate = "online";
+var hi; // this will be undefined
+
+console.table([accountId, accountEmail, accountPassword, accountstate, hi]);
+
+// never use var
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all the JS code as newer version.....
+// Dont need to write it every time, it is executed everytime nowadadys
+
+// number => 2 to power 53
+// bigint
+//string =>""
+//boolean =>true/false
+// null =>standalone vale
+//undefined =>
+//----** Remenber  null and undefined are not the same rem
+
+
+// Interview Questions
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
