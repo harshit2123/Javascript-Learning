3e6d3c06984e72675ab902bd175733e1ca9a7d91
Harshit kumar yadav <harshitkmryadav@gmail.com>
2025-02-21 18:18:28 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = 33;
+
+//to check what is the type of the value
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// "33" => 33 // aram se convert ho jate hai
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn =1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 =>false
+// "" => false
+// "hitesh" => true
+
+// Jo browser hai wo hme zadatar string values deta hai...hme use bhot baar 
+// number, boolean, object wagera me convert krna padta hai
